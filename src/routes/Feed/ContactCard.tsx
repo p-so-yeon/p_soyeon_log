c78dc17d981c94820b4fc5da8c58cc503906750e
may <soyeon1056@naver.com>
2023-10-17 17:31:42 +0900

    error 수정

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -44,16 +44,7 @@ const ContactCard: React.FC = () => {
             <div className="name">email</div>
           </a>
         )}
-        {CONFIG.profile.linkedin && (
-          <a
-            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillLinkedin className="icon" />
-            <div className="name">linkedin</div>
-          </a>
-        )}
+   
       </StyledWrapper>
     </>
   )
@@ -71,7 +62,7 @@ const StyledWrapper = styled.div`
   flex-direction: column;
   border-radius: 1rem;
   background-color: ${({ theme }) =>
-    theme.scheme === "light" ? "white" : theme.colors.gray4};
+    theme.scheme === "light" ? "white" : theme.colors.gray7};
   a {
     display: flex;
     padding: 0.75rem;
